6587ca60a1f026bd0fdff858113db4f1dffffffd
Shou Ya <shouyatf@gmail.com>
2012-10-03 22:04:29 +0800

    it now works

--- app.rb
+++ app.rb
@@ -6,32 +6,46 @@ require 'json'
 require_relative 'dbinterface'
 
 before do
-  @dbi = connect_database
+  @dbi = Database.new
 end
 
 get '/' do
+  redirect to('/main.html')
+end
+
+get '/main.html' do
   erb :mainpage
 end
 
-post '/rate' do
-  params['winner']
-  params['loster']
+post '/vote' do
+#  params['id']
 end
 
 get '/refresh' do
   content_type :json
-  return JSON.dump({
-                     :pic_left => {
-                       :data => Base64.encode64(File.read('temp/0111348.jpg')),
-                       :id => 1,
-                       :info => 'Katie'
-                     },
-                     :pic_right => {
-                       :data => Base64.encode64(File.read('temp/0111437.jpg')),
-                       :id => 3,
-                       :info => 'Lily'
-                     }
-                   })
+
+  json_hash = @dbi.random_pick
+
+  json_hash.each do |_,v|
+    v[:data] = Base64.encode64 File.read(File.join('photo',
+                                                   v.delete(:barcode) + '.jpg'))
+
+  end
+=begin
+  {
+    :pic_left => {
+      :data => Base64.encode64(File.read('temp/0111348.jpg')),
+      :id => 1,
+      :info => 'Katie'
+    },
+    :pic_right => {
+      :data => Base64.encode64(File.read('temp/0111437.jpg')),
+      :id => 3,
+      :info => 'Lily'
+    }
+  }
+=end
+  return JSON.dump(json_hash)
 end
 
 
--- dbinterface.rb
+++ dbinterface.rb
@@ -3,40 +3,82 @@ require 'mysql'
 require_relative 'dbconfig'
 
 
-@db_conn = nil
 
-def create_tables(conn)
-  conn.query(<<HERE)
-CREATE TABLE student_info (
+
+class Database
+  attr :db_conn
+
+  TABLE_NAME = 'student_info'
+
+  def initialize
+    @db_conn = get_connection
+    create_table unless @db_conn.list_tables.include? 'student_info'
+  end
+
+  def get_connection
+    @db_conn || connect_database
+  end
+
+  def create_table
+    @db_conn.query(<<HERE)
+CREATE TABLE #{TABLE_NAME} (
    id INT PRIMARY KEY AUTO_INCREMENT,
    name VARCHAR(255) NOT NULL,
    barcode VARCHAR(20) NOT NULL,
-   class VARCHAR(20)
+   other_info VARCHAR(20),
+   rating INT
 );
 HERE
 
-end
+  end
 
-def connect_database
-  return @db_conn if @db_conn
+  def connect_database
+    Mysql.connect(MYSQL_HOST, MYSQL_USERNAME,
+                  MYSQL_PASSWORD, MYSQL_DBNAME)
+  end
 
-  @db_conn = Mysql.connect(MYSQL_HOST, MYSQL_USERNAME,
-                           MYSQL_PASSWORD, MYSQL_DBNAME)
-
-  create_tables(@db_conn) unless  @db_conn.list_tables.include? 'student_info'
-
-  return @db_conn
-end
-
-def insert_data(name, barcode, clazz = nil)
-  pst = @db_conn.prepare(<<HERE)
-INSERT INTO student_info
-  (name, barcode, class)
+  def insert_data(name, barcode, other_info = nil)
+    pst = @db_conn.prepare(<<HERE)
+INSERT INTO #{TABLE_NAME}
+  (name, barcode, other_info, rating)
 VALUES
-  (?, ?, ?);
+  (?, ?, ?, 0);
 HERE
 
-  pst.execute(name, barcode, clazz || '')
+    pst.execute(name, barcode, other_info || '')
+  end
 
+  def vote_student(id)
+    pst = @db_conn.prepare(<<HERE)
+UPDATE #{TABLE_NAME}
+SET rating = rating + 1
+WHERE id = ?
+HERE
+
+    pst.execute(id.to_i)
+  end
+
+  def random_pick
+    tbl_rst = @db_conn.query(<<HERE)
+SELECT id, name, other_info, barcode
+FROM #{TABLE_NAME}
+ORDER BY RAND()
+LIMIT 0, 2
+HERE
+
+    result = {}
+
+    [:pic_left, :pic_right].each do |side|
+      row = tbl_rst.fetch_row
+
+      result.merge!(side => {})
+      result[side].merge!(:id => row[0].to_i,
+                          :info => row[1] + (row[2] ? ", #{row[2]}" : nil).to_s,
+                          :barcode => row[3].to_s)
+    end
+
+    return result
+  end
 
 end
+
